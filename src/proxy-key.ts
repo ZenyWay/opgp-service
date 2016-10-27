c6f2b6a10c150e8b9778b46c149c470548aafd1f
S. M. Catala <stephane@zenyway.com>
2016-10-28 01:05:09 +0200

    commit missing OpgpProxyKey declarations

---
+++ src/proxy-key.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Stephane M. Catala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
+;
+export interface OpgpProxyKey extends OpgpKeyBlueprint {
+  handle: string
+}
+
+/**
+ *
+ */
+export interface OpgpKeyBlueprint {
+  isLocked: boolean
+  isPublic: boolean
+  keys: OpgpKeyId[]
+  user: OpgpKeyUser
+}
+
+export interface OpgpKeyId {
+  isAuth: boolean
+  isCiph: boolean
+  expires: number
+  fingerprint: string
+  hash: string
+  status: number
+}
+
+export interface OpgpKeyUser {
+  ids: string[]
+}
